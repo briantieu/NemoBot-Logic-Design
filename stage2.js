a230e41272826a6c1d2a33ecb0a89cbc85364873
briantieu <34044831+briantieu@users.noreply.github.com>
2020-12-30 03:18:10 -0600

    Update stage2.js

--- stage2.js
+++ stage2.js
@@ -1,4 +1,7 @@
 'use strict';
+
+
+
 const start = (say, sendButton) => {
 	say('A Boolean expression is a sentence consisting of Boolean variables, constants, and operators.').then(() => {
 	    sendButton('Which of the following is a Boolean variable?', [{title: 'True', payload: '0-0'}, {title: 'a', payload: '0-1'}, {title: 'AND', payload: '0-2'}]);
@@ -12,25 +15,21 @@ const state = (payload, say, sendButton) => {
     
     switch (currentState) {
         case 0:
-            if (playerSelection === 1) { say('Correct'); }
-	        else { say('Incorrect'); }
+            checkCorrectness(say, playerSelection, 1);
 	        sendButton('Which of the following is a Boolean constant?', [{title: 'True', payload: '1-0'}, {title: 'a', payload: '1-1'}, {title: 'AND', payload: '1-2'}]);
 	    break;
 	    case 1: 
-	        if (playerSelection === 0) { say('Correct'); }
-	        else { say('Incorrect'); }
+	        checkCorrectness(say, playerSelection, 0);
 	        sendButton('Which of the following is a Boolean operator?', [{title: 'True', payload: '2-0'}, {title: 'a', payload: '2-1'}, {title: 'AND', payload: '2-2'}]);
 	    break;
 	    case 2:
-	        if (playerSelection === 2) { say('Correct'); }
-	        else { say('Incorrect'); }
+	        checkCorrectness(say, playerSelection, 2);
 	        setTimeout(say(['Let’s try evaluating a Boolean expression!','If a = TRUE, b = FALSE, c = TRUE, d = TRUE, what is:']).then(() => {
 	            sendButton('(a AND b) OR (c AND d)?', [{title: 'True', payload: '3-0'}, {title: 'False', payload: '3-1'}]);
 	        }), 1000);
 	    break;
 	    case 3:
-	        if (playerSelection === 0) { say('Correct'); }
-	        else { say('Incorrect'); }
+	        checkCorrectness(say, playerSelection, 0);
 	        say(['Instead of typing out AND, OR, and NOT every time, we can represent them in a more compact way.',
 	        'AND can be represented by *\nOR can be represented by +\nNOT can be represented by ’\nTRUE can be represented by 1\nFALSE can be represented by 0',
 	        'So, our original expression can be rewritten as: {(a * b) + (c * d), (a + b) * (c + d)}',
@@ -40,26 +39,27 @@ const state = (payload, say, sendButton) => {
             });
 	    break;
 	    case 4:
-	        if (playerSelection === 1) { say('Correct'); }
-	        else { say('Incorrect'); }
+	        checkCorrectness(say, playerSelection, 1);
 	        sendButton('F is 1 only when a is 1 and b is 0.', [{title: 'F = a * b\'', payload: '5-0'}, {title: 'F = a + b', payload: '5-1'}, {title: 'F = a\' + b\'', payload: '5-2'}]);
         break;
         case 5:
-            if (playerSelection === 0) { say('Correct'); }
-            else { say('Incorrect'); }
+            checkCorrectness(say, playerSelection, 0);
             sendButton('How about an English scenario? The bulb should light up when the switch is clicked and the power is on.',
             [{title: 'bulb = switch + power', payload: '6-0'}, {title: 'bulb = switch\' + power', payload: '6-1'}, {title: 'bulb = switch * power', payload: '6-2'}]);
         break;
         case 6:
-            if (playerSelection === 2) { say('Correct'); }
-            else { say('Incorrect'); }
+            checkCorrectness(say, playerSelection, 2);
             sendButton('Nemo Bot will be sad if you close out of the app or answer a question with an incorrect response.',
             [{title: 'sad = closed + correct\'', payload: '7-0'} , {title: 'sad = closed\' * correct', payload: '7-1'}, {title: 'sad = closed + correct', payload: '7-2'}]);
         break;
         case 7:
-            if (playerSelection === 0) { say('Correct'); }
-            else { say('Incorrect'); }
+            checkCorrectness(say, playerSelection, 0);
     }
+};
+
+const checkCorrectness = (say, a, b) => {
+    if (a === b) { say('Correct'); }
+	else { say('Incorrect'); }
 };
 
 module.exports = {
